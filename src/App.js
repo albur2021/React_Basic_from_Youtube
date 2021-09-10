d5d036fdfdd9646647f2cef2eadcad3d8f8eaa0c
Al Bur <albur2021@yahoo.com>
2021-09-10 13:40:43 -0400

    Add WithCounter Comp as Re-Used Counter Function to HoverCounter and ClickCounter Comps to share re-used methods among them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
 
+//Lift counter logic to Parent and pass props
+//Component Tree - Parent - > props -> ClickCounter and HoverCounter -> state -> Parent
+//Higher Order Components (HOC) - A pattern where a function takes a component as an argument and returns a new component
+//Example:
+// const newComponent = higherOrderComponent (originalComponent)
+// const EnhancedComponent = higherOrderComponent (originalComponent)
 function App() {
   return (
     <div className="App">
-      
-    
+      <ClickCounter />
+      <HoverCounter/>
     </div>
   );
 }
